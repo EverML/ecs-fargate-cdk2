745b044adfeed9f20ec732ff62e47e88bd86c402
Ever Leiva <eleiva@arkho.io>
2022-06-24 11:39:09 -0400

    cambio nombre creds & account number

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,4 @@
 export CDK_NEW_BOOTSTRAP=1
 npx cdk bootstrap --bootstrap-customer-key \
     --cloudformation-execution-policies 'arn:aws:iam::aws:policy/AdministratorAccess' \
-    aws://123456789012/us-east-1 \
-    --profile my_aws_profile # this should point to your aws profile name in your ~/.aws folder
+    aws://526358487936/us-east-1 --profile ever
