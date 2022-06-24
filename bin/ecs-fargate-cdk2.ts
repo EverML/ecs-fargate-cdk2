e329ee4688e64111230c4bf1492c1d6919a1b69d
Ever Leiva <eleiva@arkho.io>
2022-06-24 10:59:31 -0400

    added some explanations

--- bin/ecs-fargate-cdk2.ts
+++ bin/ecs-fargate-cdk2.ts
@@ -5,5 +5,5 @@ import { EcsFargateCdk2Stack } from '../lib/ecs-fargate-cdk2-stack';
 
 const app = new cdk.App();
 new EcsFargateCdk2Stack(app, 'EcsFargateCdk2Stack', {
-  env: { account: '526358487936', region: 'us-east-1' },
-});
+  env: { account: '123456789012', region: 'us-east-1' },
+});                 
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@
 export CDK_NEW_BOOTSTRAP=1
 npx cdk bootstrap --bootstrap-customer-key \
     --cloudformation-execution-policies 'arn:aws:iam::aws:policy/AdministratorAccess' \
-    aws://526358487936/us-east-1 --profile ever
+    aws://123456789012/us-east-1 \
+    --profile my_aws_profile # this should point to your aws profile name in your ~/.aws folder
