e53fd5308f2590821cb5d5b185f2535fceefe90d
Ever Leiva <eleiva@arkho.tech>
2023-04-08 23:09:01 -0400

    agregada imagen de recursos de networking creados

--- bin/ecs-fargate.ts
+++ bin/ecs-fargate.ts
@@ -10,17 +10,16 @@ export enum STAGE {
   PRD = 'prd' 
 }
 
-
 const app = new cdk.App();
 
-
 //BASE VPC AND NETWORKING RESOURCES
-const vpcDEV = new VPCStack(app,`VPCSTACK${STAGE.DEV}`,{
+const vpcDEV = new VPCStack(app,`VPCSTACKDEV`,{
+  env: { account: '361941603254', region: 'us-east-1' },
   stage: STAGE.DEV,
   cidr: '10.0.0.0/16'
 })
 
-new EcsClusterStack(app, `EcsClusterStack${STAGE.DEV}`, {
+new EcsClusterStack(app, `EcsClusterStackDEV`, {
   env: { account: '361941603254', region: 'us-east-1' },
   stage: STAGE.DEV,
   vpc: vpcDEV.vpc
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 export CDK_NEW_BOOTSTRAP=1
 npx cdk bootstrap --bootstrap-customer-key \
     --cloudformation-execution-policies 'arn:aws:iam::aws:policy/AdministratorAccess' \
-    aws://526358487936/us-east-1 --profile ever
+    aws://361941603254/us-east-1
